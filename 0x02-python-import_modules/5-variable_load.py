dff1c05fd00812cfb13dad02854a119d79c9ebbe
MadukaJustice <jonasjustice98@gmail.com>
2022-12-01 14:40:07 -0800

    variable load

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# import a variable from a module
+
+from variable_load_5 import a
+
+
+def print_a():
+    print("{:d}".format(a))
+
+
+if __name__ == "__main__":
+    print_a()
