02970deb44ccbf7da4521589ff1f931931109573
MadukaJustice <jonasjustice98@gmail.com>
2022-12-02 17:25:26 +0000

    prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            if col != row[len(row) - 1]:
+                print("{:d}".format(col), end=" ")
+            else:
+                print("{:d}".format(col), end="")
+        print()
