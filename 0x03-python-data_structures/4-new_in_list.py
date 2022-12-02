8e0121bf744509c1f8fe6866cbc9866a5832e32e
MadukaJustice <jonasjustice98@gmail.com>
2022-12-02 14:50:17 +0000

    new in list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_copy = my_list.copy()
+    if idx < 0 or idx > (len(my_list) - 1):
+        return new_copy
+    else:
+        new_copy[idx] = element
+        return new_copy
