612d6c3ccd9120afe089534aef3152fed0939944
MadukaJustice <jonasjustice98@gmail.com>
2022-12-19 13:03:52 -0800

    python exceptions

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    items = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            items += 1
+
+        except (TypeError, ValueError):
+            continue
+
+    print()
+    return (items)
