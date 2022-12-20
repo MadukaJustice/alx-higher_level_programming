7a60fa64ecd76d1180ac91acbfc7b93dded83d41
MadukaJustice <jonasjustice98@gmail.com>
2022-12-20 15:07:13 -0800

    square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""
+    Defines a class Square with private attribute
+"""
+
+
+class Square:
+    """
+        Definition with __init__ size
+    """
+
+    def __init__(self, size=0):
+        """
+            Initialize Square class
+
+            Attributes:
+                size: size of the square of type int
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+            Returns current Square size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+            Property setter for size attribute
+        """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+            Returns current square area
+        """
+        return (self.__size**2)
+
+    def my_print(self):
+        """
+            Prints # square times
+        """
+
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+
+        if self.__size == 0:
+            print("")
