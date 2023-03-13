ed7fd29ad64729bcb515059a49eb2029f66f5b68
MadukaJustice <jonasjustice98@gmail.com>
2023-03-13 23:47:47 +0000

    mandatory tasks done and dusted

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,22 @@
 #!/usr/bin/node
+// searches the second biggest integer in the list of arguments
 
-const arr = process.argv.slice(2);
-function secondBiggest (arr) {
-  if (arr.length < 2) {
+const getSecondMax = (arr) => {
+  let max = parseInt(arr[2]);
+  let secMax = parseInt(arr[2]);
+  
+  if ((arr.length === 2) || (arr.length === 3)) {
     return (0);
-  } else {
-    arr.sort((i, j) => i - j);
-    arr.pop();
-    return (arr.pop());
   }
-}
-console.log(secondBiggest(arr));
+    for (let i = 3; i < arr.length; i++) {
+      if (parseInt(arr[i]) >= max) {
+        secMax = max;
+        max = parseInt(arr[i]);
+      } else if (parseInt(arr[i]) > secMax) {
+        secMax = parseInt(arr[i]);
+      }
+    }
+    return (secMax);
+  };
+
+console.log(getSecondMax(process.argv));
