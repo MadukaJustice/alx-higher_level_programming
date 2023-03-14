331dd05243246b75555d8fa56c28d710407e3b24
MadukaJustice <jonasjustice98@gmail.com>
2023-03-14 18:54:37 +0000

    task 0 to 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Converts a number from base 10
+// to another base passed as argument
+
+exports.converter = function (base) {
+  return num => num.toString(base);
+};
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+// A class ``Rectangle`` that defines a rectangle
+
+class Rectangle {
+  constructor (w,h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate() {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
