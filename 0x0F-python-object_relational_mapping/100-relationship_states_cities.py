6c6fba06913e51126b534791434ca989675cd92b
MadukaJustice <jonasjustice98@gmail.com>
2023-03-18 10:24:28 +0000

    advanced tasks using sqlachemy

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+create state "California" with city attribute "San Francisco"
+parameters given: username, password, database
+"""
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # make engine for db
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="California")
+    new_city = City(name="San Francisco")
+    new_state.cities.append(new_city)
+
+    session.add(new_state)
+    session.add(new_city)
+
+    session.commit()
+    session.close()
