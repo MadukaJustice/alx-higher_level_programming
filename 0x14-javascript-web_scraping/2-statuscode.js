78b698756c4a029dc853cae743a6722e37bc57fe
MadukaJustice <jonasjustice98@gmail.com>
2023-04-25 21:02:52 +0000

    A script that display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`code: ${res.statusCode}`);
+});
